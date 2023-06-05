f2f6d557ba707cdfd7527f4fd33ee4dc54d36cea
= <=>
2023-06-05 16:39:08 -0400

    agregando adp

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import './App.css';
-import ListaDeTareas from './componentes/ListaDeTareas';
-
-
+import "./App.css";
+import ListaDeTareas from "./componentes/ListaDeTareas";
 
 function App() {
-  return (
-    <div className='aplicacion-tareas'>
-            <div className='tareas-lista-principal'>
-        <h1>Mis Tareas</h1>
-        
-        <ListaDeTareas />
-      
-      </div>
-    </div>
-  );
+	return (
+		<div className="aplicacion-tareas">
+			<div className="tareas-lista-principal">
+				<h1>Mis Tareas klk</h1>
+
+				<ListaDeTareas />
+			</div>
+		</div>
+	);
 }
 
-
-export default App
+export default App;
